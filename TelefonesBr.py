47b4e2f8d11ced3c2d0eb79bee54c35d89eeb841
Rafael Telles <rafast.telles@gmail.com>
2021-09-25 12:13:03 -0300

    Corrigindo formatação para números com código do pais

--- TelefonesBr.py
+++ TelefonesBr.py
@@ -17,15 +17,23 @@ class TelefonesBr:
             return False
 
     def format_numero(self):
-        padrao = '([0-9]{2,3})?([0-9]{2})([0-9]{4,5})([0-9]{4})'
+        padrao = '([0-9]{2})?([0-9]{2})([0-9]{4,5})([0-9]{4})'
         resposta = re.search(padrao, self.numero)
-        numero_formatado = '{}({}){}-{}'.format(
-            '+'+resposta.group(1) if resposta.group(1) != None else '',
+        if resposta.group(1) == None:
+            numero_formatado = '({}){}-{}'.format(
+                resposta.group(2),
+                resposta.group(3),
+                resposta.group(4)
+            )
+            return numero_formatado
+        else:
+            numero_formatado = '+{}({}){}-{}'.format(
+            resposta.group(1),
             resposta.group(2),
             resposta.group(3),
             resposta.group(4)
-        )
-        return numero_formatado
+            )
+            return numero_formatado
 
     def __str__(self):
         return self.format_numero()
